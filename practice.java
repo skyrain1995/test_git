5d7fdd08b776bf26d6af172ca43d456d770e3e9d
skyrain1995 <2503123003@qq.com>
2019-01-24 11:43:22 +0800

    test code commit

---
+++ practice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class practice {
+    public static void main(String[] args){
+        int[] a = {4,3,2,1};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+
+
+}
